b9e0111605fedd303cdb80ad08cd6d828a8db818
linmb@qq.com <linmb@qq.com>
2017-05-18 06:31:45 +0800

    firstservlet

---
+++ src/com/bingley/servlet/MyFirstServlet.java
@@ -0,0 +1,27 @@
+package com.bingley.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Mr.bingley
+ * @version 1.0.0
+ * @des 要理解从零建servlet过程
+ * @since 2017/5/18.
+ */
+@WebServlet(name = "MyFirstServlet")
+public class MyFirstServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("now time"+new Date().toLocaleString());
+    }
+}
